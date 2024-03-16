fc7b628d173d68169d9397b9090f2ad1a1016e10
whoischrislam <whoischrislam@gmail.com>
2024-03-15 18:00:49 -1000

    Basic setup

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});

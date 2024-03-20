e3bf392b85b94c6ab061d9db8a183cd024415d3a
whoischrislam <whoischrislam@gmail.com>
2024-03-20 07:47:32 -1000

    Basic feature complete

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-const posts = [];
+let idCount = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: 'Initial Post of my new blog!',
+    content: 'I just wanted to share to the world this blog! I hope you like it!',
+    dateTime: new Date().toLocaleString()
+  }
+];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,13 +24,41 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit', (req, res) => {
+    idCount++;
     const newPost = {
+      id: idCount,
       title: req.body.postTitle,
       content: req.body.postContent,
       dateTime: new Date().toLocaleString()
     };
     posts.unshift(newPost);
+    // console.log(`New post added: ID: ${newPost.id}|| TITLE: ${newPost.title}|| CONTENT: ${newPost.content}|| DATE: ${newPost.dateTime}`);
     res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+  const deletePostId = parseInt(req.body.deletePostId);
+  const deleteArray = posts.find(element => element.id === deletePostId);
+  const indextoDelete = posts.indexOf(deleteArray);
+  posts.splice(indextoDelete, 1);
+  res.redirect('/');
+});
+
+app.post('/edit', (req, res) => {
+  // Take request and create a new post in preparation for replacing the old post
+  const editPost = {
+    id: parseInt(req.body.editPostId),
+    title: req.body.editPostTitle,
+    content: req.body.editPostContent,
+    dateTime: "Edited on " + new Date().toLocaleString()
+  };
+
+  // Find the post to edit and replace it with the new post
+  const editArray = posts.find(element => element.id === editPost.id);
+  const indextoEdit = posts.indexOf(editArray); // Get the index of the post to edit
+  posts[indextoEdit] = editPost; // Replace the old post with the new post
+
+  res.redirect('/');
 });
 
 app.listen(port, () => {
